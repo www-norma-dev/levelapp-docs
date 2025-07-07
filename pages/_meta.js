494fc231fbbf697a3720b4e8a8bed6d4eb48c760
bee11111 <21017@supnum.mr>
2025-07-07 06:52:41 +0000

    First Commit: Setup Nextra V3 documentation project

---
+++ pages/_meta.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  index: { title: "Home" },
+  about: { title: "About This Project" },
+  contact: {
+    title: "Contact Us",
+    type: "doc",
+    navigation: {
+      prev: false,
+      next: false,
+    },
+  },
+  guides: { title: "Guides" },
+};
---
+++ pages/guides/_meta.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  gettingStarted: "Getting Started",
+  advancedTips: "Advanced Tips",
+};
