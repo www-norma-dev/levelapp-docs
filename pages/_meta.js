5fb1ac399b54959b59eca1fee36d863fab12e4f0
Bedra11 <bedradedi@gmail.com>
2025-07-07 16:17:11 +0000

    Add guides/index.mdx with summary on Nextra usage and SEO setup

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,14 +1,41 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  index: { title: "Home" },
-  about: { title: "About This Project" },
-  contact: {
-    title: "Contact Us",
-    type: "doc",
-    navigation: {
-      prev: false,
-      next: false,
+  index: {
+    title: "Home",
+    theme: {
+      // breadcrumb: false,
+      // toc: false,
+      // layout: "full",
+      // typesetting: "article",
     },
   },
+  // "---": {
+  //   type: "separator",
+  // },
+
+  about: { title: "About This Project", type: "page" },
+  contact: {
+    title: "Contact Us",
+    // display: "hidden",
+  },
   guides: { title: "Guides" },
+  github: {
+    title: "GitHub",
+    href: "https://github.com",
+    newWindow: true,
+  },
+  resources: {
+    title: "Resources",
+    type: "menu",
+    items: {
+      github: {
+        title: "GitHub",
+        href: "https://github.com",
+      },
+      contact: {
+        title: "Contact Us",
+        href: "mailto:hi@example.com",
+      },
+    },
+  },
 };
--- pages/guides/_meta.js
+++ pages/guides/_meta.js
@@ -1,5 +1,4 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  gettingStarted: "Getting Started",
-  advancedTips: "Advanced Tips",
+  index: "Nextra Setup Guide",
 };
