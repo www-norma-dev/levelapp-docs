c1043d218bd5424c5e00cb3f5c3f2132c352ad03
mdmahmoud32 <23215@esp.mr>
2025-08-04 14:29:27 +0000

    Add core concepts documentation (architecture, simulators, evaluators, etc.)

---
+++ pages/core-concepts/_meta.js
@@ -0,0 +1,21 @@
+// pages/core-concepts/_meta.js
+export default {
+  index: {
+    title: "Overview",
+  },
+  architecture: {
+    title: "Architecture",
+  },
+  simulators: {
+    title: "Simulators",
+  },
+  evaluators: {
+    title: "Evaluators",
+  },
+  scoring: {
+    title: "Scoring",
+  },
+  "test-batches": {
+    title: "Batch Tests",
+  },
+};
