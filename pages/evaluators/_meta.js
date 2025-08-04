8840b1c84f23913e2e28dc0ac0d2166b48021dcd
AnasAmchaar <anass.amchaar14@gmail.com>
2025-08-04 11:52:07 +0100

    Adding the Evaluators part to the documentation of LevelApp

---
+++ pages/evaluators/_meta.js
@@ -0,0 +1,17 @@
+export default {
+    index: {
+        title: "Overview",
+    },
+    "common-format": {
+        title: "Common Input/Output Format",
+    },
+    openai: {
+        title: "OpenAI Evaluators",
+    },
+    ionos: {
+        title: "IONOS Evaluators", 
+    },
+    "custom-evaluators": {
+        title: "Custom Evaluators",
+    }
+}
